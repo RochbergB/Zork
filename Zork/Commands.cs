640ae6728b153ca71e674cdfe024ad9cfbdfbb5b
RochbergB <rochbergb@knights.ucf.edu>
2022-08-29 15:22:46 -0400

    Zork 1.2 Initial Commit

---
+++ Zork/Commands.cs
@@ -0,0 +1,15 @@
+﻿namespace Zork
+{
+    public enum Commands
+    { 
+        Quit = 0,
+        Look,
+        North,
+        South,
+        East,
+        West,
+        Unknown
+    }
+
+
+}
--- Zork/Program.cs
+++ Zork/Program.cs
@@ -6,21 +6,44 @@ namespace Zork
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Zork!");
-
+            Console.WriteLine("Welcome to Zork!");        
             string inputString = Console.ReadLine().Trim().ToUpper();
-            if (inputString == "QUIT")
+            Commands command = ToCommand(inputString);
+
+            if (command == Commands.Quit)
             {
                 Console.WriteLine("Thank you for playing.");
             }
-            else if  (inputString == "LOOK")
+            else if  (command == Commands.Look)
             {
                 Console.WriteLine("This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door.");
+            }
+            else if (command == Commands.North)
+            {
+                Console.WriteLine("You went North.");
+            }
+            else if (command == Commands.South)
+            {
+                Console.WriteLine("You went South.");
+            }
+            else if (command == Commands.East)
+            {
+                Console.WriteLine("You went East.");
+            }
+            else if (command == Commands.West)
+            {
+                Console.WriteLine("You went West.");
             }
             else
             {
                 Console.WriteLine($"Unrecognized command: {inputString}");
             }
+        }
+
+        static Commands ToCommand(string commandString)
+        {
+            return Enum.TryParse<Commands>(commandString, true, out Commands command) ? command : Commands.Unknown;
+           
         }
     }
 }
