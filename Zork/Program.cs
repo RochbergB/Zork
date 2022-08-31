cd7d7e00579656fbb1e93dca09015930cee15799
RochbergB <rochbergb@knights.ucf.edu>
2022-08-31 15:18:16 -0400

    Zork 1.3 Initial Commit

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -8,9 +8,39 @@ namespace Zork
         {
             Console.WriteLine("Welcome to Zork!");
 
-            string inputString = Console.ReadLine();
-            Commands command = ToCommand(inputString.Trim().ToUpper());
-            Console.WriteLine(command);
+            bool isRunning = true;
+            while (isRunning)
+            {
+                Console.Write("> ");
+                string inputString = Console.ReadLine().Trim();
+                Commands command = ToCommand(inputString);
+                
+                string outputString;
+                switch (command)
+                {
+                    case Commands.Quit:
+                        isRunning = false;
+                        outputString = "Thank you for playing!";
+                        break;
+
+                    case Commands.Look:
+                        outputString = "This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        break;
+
+                    case Commands.North:
+                    case Commands.South:
+                    case Commands.East:
+                    case Commands.West:
+                        outputString = $"You moved {command}.";
+                        break;
+
+                    default:
+                        outputString = "Unknown command.";
+                        break;
+                }
+
+                Console.WriteLine(outputString);
+            }
         }
         private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.Unknown;
 
