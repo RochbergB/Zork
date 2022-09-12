447e37b7ab8b0ea4dea242cfaad54741cf6feb46
RochbergB <rochbergb@knights.ucf.edu>
2022-09-12 14:52:13 -0400

    Zork 2.2 Initial Commit

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,20 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Zork
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static string CurrentRoom
+        {
+            get
+            {
+                return _rooms[_location.Row, _location.Column];
+            }
+        }
+
+        private static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
 
             bool isRunning = true;
             while (isRunning)
             {
-                Console.Write($"{_rooms[_currentRoom]}\n> ");
+                Console.Write($"{CurrentRoom}\n> ");
                 string inputString = Console.ReadLine().Trim();
                 Commands command = ToCommand(inputString);
-                
+
                 string outputString;
                 switch (command)
                 {
@@ -37,7 +46,7 @@ namespace Zork
                         }
                         else
                         {
-                                outputString = "The way is shut!";
+                            outputString = "The way is shut!";
                         }
                         break;
 
@@ -56,25 +65,35 @@ namespace Zork
 
             switch (command)
             {
-                case Commands.North:
-                case Commands.South:
+                case Commands.North when _location.Row < _rooms.GetLength(0) - 1:
+                    _location.Row++;
+                    didMove = true;
                     break;
 
-                case Commands.East when _currentRoom < _rooms.Length - 1:
-                    _currentRoom++;
-                    didMove = true;                 
+                case Commands.South when _location.Row > 0:
+                    _location.Row--;
+                    didMove = true;
                     break;
 
-                case Commands.West when _currentRoom > 0:
-                    _currentRoom--;
+                case Commands.East when _location.Column < _rooms.GetLength(1) - 1:
+                    _location.Column++;
+                    didMove = true;
+                    break;
+
+                case Commands.West when _location.Column > 0:
+                    _location.Column--;
                     didMove = true;
                     break;
             }
             return didMove;
         }
 
-        private static readonly string[] _rooms = { "Forest", "West of House", "Behind House", "Clearing", "Canyon View" };
-        private static int _currentRoom = 1;
+        private static readonly string[,] _rooms = {
+            { "Rocky Trail", "South of House", "Canyon View" },
+            { "Forest", "West of House", "Behind House" }, 
+            { "Dense Woods", "North of House", "Clearing" }
+        };
 
+        private static (int Row, int Column) _location = (1, 1);
     }
 }
