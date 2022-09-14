d789169eed411294a5ed3cae3daa4392da6622a3
RochbergB <rochbergb@knights.ucf.edu>
2022-09-14 15:00:59 -0400

    Zork 3.1 Small Code Edits

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace Zork
 {
@@ -100,12 +99,13 @@ namespace Zork
             _rooms[1, 1].Description = "This is an open field west of a white house, with a boarded front door.";                                       // West of House
             _rooms[1, 2].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly ajar.";    // Behind House
 
-            _rooms[2, 0].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";           //Dense Woods
+            _rooms[2, 0].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";           // Dense Woods
             _rooms[2, 1].Description = "You are facing thhe north side of a white house. There is no door here, and all the windows are barred.";       // North of House
             _rooms[2, 2].Description = "You are in a clearing, with a forest surrounding you on the west and south.";                                   // Clearing
         }
 
-        private static readonly Room[,] _rooms = {
+        private static readonly Room[,] _rooms = 
+        {
             { new Room("Rocky Trail"), new Room("South of House"), new Room("Canyon View") },
             { new Room("Forest"), new Room("West of House"), new Room("Behind House") }, 
             { new Room("Dense Woods"), new Room("North of House"), new Room("Clearing") }
