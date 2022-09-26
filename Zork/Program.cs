ed39cf49dce2f2fd98cdf98cccd32f087c218485
Ben Rochberg <rochbergb@knights.ucf.edu>
2022-09-26 15:28:22 -0400

    Zork 4.1 Updated InitializeRoomDescription Method

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,17 +1,148 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Zork
 {
     internal class Program
-    {      
+    {
+        private static Room CurrentRoom
+        {
+            get
+            {
+                return _rooms[_location.Row, _location.Column];
+            }
+        }
+
         private static void Main(string[] args)
         {
+            string roomeFilename = args.Length > 0 ? args[(int)CommandLineArguments.RoomsFilename] : @"Content\Rooms.txt";
+            InitializeRoomDescription(roomeFilename);
+
             Console.WriteLine("Welcome to Zork!");
 
-            Game game = new Game();
-            game.Run();
-            
+            Room previousRoom = null;
+            bool isRunning = true;
+            while (isRunning)
+            {
+                Console.Write($"{CurrentRoom}\n> ");
+
+                if (previousRoom != CurrentRoom)
+                {
+                    Console.WriteLine(CurrentRoom.Description);
+                    previousRoom = CurrentRoom;
+                }
+
+                string inputString = Console.ReadLine().Trim();
+                Commands command = ToCommand(inputString);
+
+                string outputString;
+                switch (command)
+                {
+                    case Commands.Quit:
+                        isRunning = false;
+                        outputString = "Thank you for playing!";
+                        break;
+
+                    case Commands.Look:
+                        outputString = CurrentRoom.Description;
+                        break;
+
+                    case Commands.North:
+                    case Commands.South:
+                    case Commands.East:
+                    case Commands.West:
+                        if (Move(command))
+                        {
+                            outputString = $"You moved {command}.";
+                        }
+                        else
+                        {
+                            outputString = "The way is shut!";
+                        }
+                        break;
+
+                    default:
+                        outputString = "Unknown command.";
+                        break;
+                }
+                Console.WriteLine(outputString);
+            }
         }
+        private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.Unknown;
+
+        private static bool Move(Commands command)
+        {
+            bool didMove = false;
+
+            switch (command)
+            {
+                case Commands.North when _location.Row < _rooms.GetLength(0) - 1:
+                    _location.Row++;
+                    didMove = true;
+                    break;
+
+                case Commands.South when _location.Row > 0:
+                    _location.Row--;
+                    didMove = true;
+                    break;
+
+                case Commands.East when _location.Column < _rooms.GetLength(1) - 1:
+                    _location.Column++;
+                    didMove = true;
+                    break;
+
+                case Commands.West when _location.Column > 0:
+                    _location.Column--;
+                    didMove = true;
+                    break;
+            }
+            return didMove;
+        }
+        private static void InitializeRoomDescription(string roomsFilename)
+        {
+            Dictionary<string, Room> roomMap = new Dictionary<string, Room>();
+            foreach (Room room in _rooms)
+            {
+                roomMap.Add(room.Name, room);
+            }
+
+            string[] lines = File.ReadAllLines(roomsFilename);
+            foreach (string line in lines)
+            {
+                const string fieldDelimiter = "##";
+                const int expectedFieldCount = 2;
+
+                string[] fields = line.Split(fieldDelimiter);
+                if (fieldDelimiter.Length != expectedFieldCount)
+                {
+                    throw new InvalidDataException("Invalid record.");
+                }
+
+                string name = fields[(int)Fields.Name];
+                string description = fields[(int)Fields.Description];
+
+                roomMap[name].Description = description;
+            }
+        }
+        private enum Fields
+        {
+            Name = 0,
+            Description
+        }
+
+        private enum CommandLineArguments
+        {
+            RoomsFilename = 0
+        }
+
+        private static readonly Room[,] _rooms =
+        {
+            { new Room("Rocky Trail"), new Room("South of House"), new Room("Canyon View") },
+            { new Room("Forest"), new Room("West of House"), new Room("Behind House") },
+            { new Room("Dense Woods"), new Room("North of House"), new Room("Clearing") }
+        };
+
+        private static (int Row, int Column) _location = (1, 1);
     }
 }
