9d67caf77b56a76aa96a9cfa2f1dbd35e4c34536
RochbergB <rochbergb@knights.ucf.edu>
2022-11-14 15:21:05 -0500

    Zork.Unity Input & Output Services

---
+++ Zork.Unity/Assets/Scripts/UnityOutputService.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using Zork.Common;
+using TMPro;
+
+public class UnityOutputService : MonoBehaviour, IOutputService
+{
+    public void Write(object obj)
+    {
+
+    }
+
+    public void Write(string message)
+    {
+
+    }
+
+    public void WriteLine(object obj)
+    {
+
+    }
+
+    public void WriteLine(string message)
+    {
+
+    }
+}
